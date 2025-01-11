609a6a4b54233e12321155e39cd226dc128e6449
Shantal Soriano <ShantalSoriano05@gmail.com>
2025-01-11 13:03:07 -0400

    Add new Panel controls to Form1
    Introduce greenPanel, imgPanel, and loginPanel to Form1.
    Configure greenPanel with background color and child panels.
    Dock imgPanel to the left and loginPanel to the right of greenPanel.
    Adjust form's client size and add greenPanel to controls.
    Resume layout logic for greenPanel after modifications.
    Add private fields for the new panels in Form1 class.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
+            greenPanel = new Panel();
+            imgPanel = new Panel();
+            loginPanel = new Panel();
+            greenPanel.SuspendLayout();
             SuspendLayout();
+            // 
+            // greenPanel
+            // 
+            greenPanel.BackColor = Color.FromArgb(22, 97, 58);
+            greenPanel.Controls.Add(loginPanel);
+            greenPanel.Controls.Add(imgPanel);
+            greenPanel.Location = new Point(33, 34);
+            greenPanel.Name = "greenPanel";
+            greenPanel.Size = new Size(761, 378);
+            greenPanel.TabIndex = 0;
+            // 
+            // imgPanel
+            // 
+            imgPanel.Dock = DockStyle.Left;
+            imgPanel.Location = new Point(0, 0);
+            imgPanel.Name = "imgPanel";
+            imgPanel.Size = new Size(350, 378);
+            imgPanel.TabIndex = 0;
+            // 
+            // loginPanel
+            // 
+            loginPanel.Dock = DockStyle.Right;
+            loginPanel.Location = new Point(394, 0);
+            loginPanel.Name = "loginPanel";
+            loginPanel.Size = new Size(367, 378);
+            loginPanel.TabIndex = 1;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(854, 474);
+            ClientSize = new Size(838, 455);
+            Controls.Add(greenPanel);
             Name = "Form1";
             Text = "Form1";
+            greenPanel.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel greenPanel;
+        private Panel loginPanel;
+        private Panel imgPanel;
     }
 }
