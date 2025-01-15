5c6c7941b07d138684152e311e049b30631fe5c7
Shantal Soriano <ShantalSoriano05@gmail.com>
2025-01-14 22:04:16 -0400

    Add DB connection class and update project dependencies
    Updated `Umbrella System.csproj` to include `Microsoft.Data.SqlClient` v5.2.2 for SQL Server connectivity. Added `DBConnection.cs` with `DBConnection` class in `Umbrella_System` namespace, featuring `ObtenerConexion` method for establishing SQL connections to `UmbrellaSystemDB` using Windows Authentication and server certificate trust.

---
+++ DBConnection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Umbrella_System
+{
+    public class DBConnection
+    {
+        public static SqlConnection ObtenerConexion()
+        {
+            SqlConnection Conexion = new SqlConnection(@"Data Source = DESKTOP - HNT4E4U\SQLEXPRESS; Initial Catalog = UmbrellaSystemDB; Integrated Security = True; Trust Server Certificate = True");
+            Conexion.Open();
+
+            return Conexion;
+        }
+    }
+}
