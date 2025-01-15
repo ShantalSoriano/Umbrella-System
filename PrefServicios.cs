b29d1c5559c2b004347d8df06e6647797d28a3f7
Shantal Soriano <ShantalSoriano05@gmail.com>
2025-01-15 01:24:27 -0400

    Add PrefServicios class with constructors and properties
    Introduce PrefServicios.cs in Umbrella_System namespace.
    - Add idCliente and idServicio properties.
    - Implement parameterless and parameterized constructors.
    - Import necessary namespaces.

---
+++ PrefServicios.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Umbrella_System
+{
+    public class PrefServicios
+    {
+        public int idCliente { get; set; }
+        public int idServicio { get; set; }
+        public PrefServicios() { }
+        public PrefServicios(int idCliente, int idServicio)
+        {
+            this.idCliente = idCliente;
+            this.idServicio = idServicio;
+        }
+    }
+}
